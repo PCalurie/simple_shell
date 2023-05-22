1107eae695ab479803ea589b6fd1f3cb9bef5734
flipblip <wickynjy@gmail.com>
2023-05-22 13:56:51 -0700

    betty setenv

--- setenv.c
+++ setenv.c
@@ -1,29 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "shell.h"
-/**
- * execute_setenv - handles the setenv command
- * @num_args: number of arguments
- * @args: array of arguments
- */
-void execute_setenv(int num_args, char **args)
-{
-	const char *name = args[1];
-	const char *value;
 
-	if (num_args < 2)
+/**
+ * setenvShell - set env var
+ * @variable: var to set
+ * @value: value to set
+ * Return: result
+ */
+
+int setenvShell(char *variable, char *value)
+{
+	int result = setenv(variable, value, 1);
+
+	if (result == -1)
 	{
-		fprintf(stderr, "setenv: Too few arguments\n");
-	}
-	else if (num_args > 3)
-	{
-		fprintf(stderr, "setenv: Too many arguments\n");
+		fprintf(stderr, "Failed to set env var.\n");
 	}
 	else
 	{
-		value = (num_args == 3) ? args[2] : "";
-
-		if (setenv(name, value, 1) != 0)
-		{
-			fprintf(stderr, "setenv: Failed to set environment variable\n");
-		}
+		printf("Environment variable '%s' set to '%s'.\n",
 	}
+
+	return (result);
 }
